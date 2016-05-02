54da52cb61b73cb62c18f52d8b762efe5ecf8884
Roman Kaporin <rmkaporin@gmail.com>
2016-05-02 23:50:29 +0300

    Database changed

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160502132416) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: :cascade do |t|
     t.float    "latitude"
     t.float    "longitude"
